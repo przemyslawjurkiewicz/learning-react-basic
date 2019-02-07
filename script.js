d8eda8498612c1428579a1507c2097d873686f11
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-07 18:50:42 +0100

    Add movies table

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+var movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju'
+    },
+    {
+        id: 2,
+        title: 'Król Lew',
+        desc: 'Film o królu sawanny'
+    }
+];
+
+var moviesElelments = movies.map(function (movie) {
+    return React.createElement('li', { key: movie.id },
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc)
+    );
+});
+
+
 var element = React.createElement('div', {},
     React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {},
-        React.createElement('li', {},
-            React.createElement('h2', {}, 'Harry Potter'),
-            React.createElement('p', {}, 'Film o czarodzieju')
-        ),
-        React.createElement('li', {},
-            React.createElement('h2', {}, 'Król lew'),
-            React.createElement('p', {}, 'Film o królu sawanny')
-        )
-    )
+    React.createElement('ul', {}, moviesElelments)
 );
 
 
