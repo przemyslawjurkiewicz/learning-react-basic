ab852712e5c2a654c0f1b112b96d4dd95edda492
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-07 19:07:31 +0100

    Add images

--- script.js
+++ script.js
@@ -2,19 +2,34 @@ var movies = [
     {
         id: 1,
         title: 'Harry Potter',
-        desc: 'film o czarodzieju'
+        desc: 'Film o czarodzieju',
+        img: './img/poter.jpg'
     },
     {
         id: 2,
         title: 'Król Lew',
-        desc: 'Film o królu sawanny'
+        desc: 'Film o królu sawanny',
+        img: './img/lew.jpg'
+    },
+    {
+        id: 3,
+        title: 'Batman',
+        desc: 'Film na podstawie komiksów DC Comics',
+        img: './img/batman.jpg'
+    },
+    {
+        id: 4,
+        title: 'Breaking Bad',
+        desc: 'Serial o nuczycielu chemii',
+        img: './img/braking.jpg'
     }
 ];
 
 var moviesElelments = movies.map(function (movie) {
     return React.createElement('li', { key: movie.id },
         React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc)
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', { src: movie.img, alt: movie.title })
     );
 });
 
